7ee45a90adc27d688529f584163cc2696cdcfa3a
“Khoi <khoi@godtsagt.no>
2021-09-22 20:24:29 +0200

    Add: ✨ Rendering posts and persons name for that post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // Get all post from the database in order and it's Laravel collection
+        $posts = Post::get();
+
+        return view('posts.index', [
+            'posts' => $posts,
+        ]);
     }
 
     // This will store the post. The $request will be used to extract the data
     public function store(Request $request ) {
+
         // Setting rules what we want to validate in our form post to store
         $this -> validate($request, [
             'body' => 'required'
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,10 +3,11 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="post">
+            <form action="{{ route('posts') }}" method="post"
+                  class="mb-4">
                 {{-- We need to add this @csrf, otherwise we get error 419 which is Laravel custom status code that warns us it's not guarding agains Cross site request forgery --}}
                 @csrf
-                <div class="mb-4">
+                <div class=" mb-4">
                     <label for="body" class="sr-only">Body</label>
                     <textarea name="body" id="body" cols="30" rows="4"
                               class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500  @enderror"
@@ -22,6 +23,19 @@
                     <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
                 </div>
             </form>
+
+            {{-- The posts will return the laravel collection --}}
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">{{ $post->user->name }}</a>
+                        <span class="text-gray-600 text-sm">Date</span>
+                        <p class="mb-2">{{ $post->body }}</p>
+                    </div>
+                @endforeach
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
