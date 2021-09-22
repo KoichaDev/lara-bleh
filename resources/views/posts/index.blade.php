4c20d42b94acd1218b6bebf5a57acbb887bd38ca
“Khoi <khoi@godtsagt.no>
2021-09-22 22:03:55 +0200

    Add: ✨ Displaying the user can see like or unlike post based if they have liked the post before

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -36,14 +36,23 @@
                         <p class="mb-2">{{ $post->body }}</p>
 
                         <div class="flex items-center">
-                            <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                            <form action="" method="post" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Unlike</button>
-                            </form>
+                            @if (!$post->likedByUser(auth()->user()))
+
+                                <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form>
+                                <form action="" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+
+                            @else
+                                <form action="" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @endif
 
                             <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                         </div>
